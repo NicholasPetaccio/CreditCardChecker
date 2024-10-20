d8a335ab171b0ae7e40c5e91f1b6aebcd20b4e25
Nick <nicholaspetaccio@gmail.com>
2024-10-20 13:00:58 -0400

    Implemented algorithm into the script to return responses on user interface.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const userInput = document.getElementById("userInput").value;
+let userInput = document.getElementById("userInput").value;
 let cc = [];
-let displayCc = [];
 
 document.getElementById('userInput').addEventListener('keydown', (e) => {
     // Allow only numbers (0-9) and the Backspace key
@@ -10,24 +9,44 @@ document.getElementById('userInput').addEventListener('keydown', (e) => {
         } else {
             cc.pop();
         }
+        validation(cc);
     } else {
         e.preventDefault();
     }
 });
 
-const validation = function luhn(array) {
-    return function (num) {
-      let len = num ? num.length : 0,
+let response = document.getElementById("responseMessage");
+let input = document.getElementById("userInput");
+
+const validation = 
+    //Immediately invoke a higher order function that returns a function that checks the credit card and validates it with the luhn algorithm.
+    function luhn(array) {
+        return function (num) {
+        let len = num ? num.length : 0,
         double = 1,
         checkSum = 0;
-
-      while (len--) {
-        checkSum += !(double ^= 1) ? parseInt(num[len], 10) : array[num[len]];
-      }
-
-      return checkSum % 10 === 0 && checkSum > 0;
+    //Loop through each digit of card from right to left and multiply every other number (XOR operator) subtract 9 when necesarry and then add the numbers together.
+        while (len--) {
+            checkSum += !(double ^= 1) ? parseInt(num[len], 10) : array[num[len]];
+        }
+    //Return the sum of all of the numbers in the array and check if it is a modulo of 10 and greater than 0.
+        if(checkSum % 10 === 0 && checkSum > 0 && cc.length > 15){
+            response.innerText = "This is a valid Credit Card number";
+            input.style.border = '2px solid green';
+            input.classList.add("valid");
+            response.style.color = "green";
+            response.classList.remove('hidden');
+            input.classList.remove('invalid');
+            response.classList.add('valid');
+        }else{
+            response.innerText = "Invalid Credit Card";
+            input.style.border = '2px solid red';
+            response.style.color = 'red';
+            response.classList.remove('hidden');
+            input.classList.add('invalid');
+        }
     };
-  }([0, 2, 4, 6, 8, 1, 3, 5, 7, 9]);
+}([0, 2, 4, 6, 8, 1, 3, 5, 7, 9]);
 
 
 
